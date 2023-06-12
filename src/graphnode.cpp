4a69d4d284c296f49c67c8d158682f24959d9d50
Chad Kaempfer <chadkaempfer@gmail.com>
2023-06-12 13:09:52 -0500

    Task 0

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+//    delete _chatBot;  // Remove to fix crash when closing Chatbot window
 
     ////
     //// EOF STUDENT CODE
